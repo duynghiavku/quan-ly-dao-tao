47dfcfac54a0a7c9c3cda9b12c9a5921d7fc88b0
duynghiadev <duynghia22302@gmail.com>
2024-04-09 17:33:55 +0700

    Thiet ke module Thoi Khoa Bieu

--- app/Http/Controllers/Frontend/Student/UserController.php
+++ app/Http/Controllers/Frontend/Student/UserController.php
@@ -7,6 +7,50 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function postNotes(Request $request)
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $date = date('d/m/Y');
+        $time = date('h:i:s');
+        $teacher_id = session('teacher_id');
+        $section_id = $request->section_id;
+        $id = $request->id;
+        $value = $request->value;
+        $reason = $id .':'. $value;
+        $attendance = Attendance::where('id_section',$section_id)->where('date',$date)->first();
+        if(isset($attendance)){
+            if($attendance->reason != null){
+                $reason_old = json_decode($attendance->reason);
+                foreach($reason_old as $key => $item){
+                    $part = explode(':',$item);
+                    $id_old = $part[0];
+
+                    if($id_old == $id){
+                        unset($reason_old[$key]);
+                    }
+                }
+            }
+            $reason_old[] = $reason;
+            $attendance->reason = json_encode(array_values($reason_old));
+            $attendance->save();
+        }else{
+            $attendance = new Attendance();
+            $attendance->id_section = $section_id;
+            $attendance->id_teacher = $teacher_id;
+            $value = Attendance::where('id_section',$section_id)->orderBy('id','desc')->first();
+            if(isset($value)){
+                $attendance->lesson = ($value->lesson)+1;
+            }else{
+                $attendance->lesson = 1;
+            }
+            $reason_new[] = $reason;
+            $attendance->reason = json_encode($reason_new);
+            $attendance->date = $date;
+            $attendance->time = $time;
+            $attendance->save();
+        }
+    }
+
     public function login(Request $request)
     {
         $email = $request->email;
